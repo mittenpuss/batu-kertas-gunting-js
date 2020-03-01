c20edd75adf6cf6a6b520bc9ab55fd190ab8b1d9
mittenpuss <42175328+mittenpuss@users.noreply.github.com>
2020-03-02 02:39:53 +0700

    Update batu-kertas-gunting.js

--- batu-kertas-gunting.js
+++ batu-kertas-gunting.js
@@ -1,6 +1,6 @@
 
 // 1 - 3
-const keluaranMusuh=(masukanPlayer)=>{
+const hompimpah=(masukanPlayer)=>{
     var musuhRandom = Math.ceil(Math.random()*3);
 
     if(musuhRandom===1){
@@ -12,14 +12,20 @@ const keluaranMusuh=(masukanPlayer)=>{
     }
 
     if((masukanPlayer==='gunting'&&masukanMusuh==='gunting')||(masukanPlayer==='kertas'&&masukanMusuh==='kertas')||(masukanPlayer==='batu'&&masukanMusuh==='batu')){
+        console.log(`anda mengeluarkan ${masukanPlayer}!`)
+        console.log(`musuh mengeluarkan ${masukanMusuh}!`)
         console.log(`anda seri, keduanya sama sama ${masukanPlayer}`)
     }else if((masukanPlayer==='gunting'&&masukanMusuh==='kertas')||(masukanPlayer==='kertas'&&masukanMusuh==='batu')||(masukanPlayer==='batu'&&masukanMusuh==='gunting')){
+        console.log(`anda mengeluarkan ${masukanPlayer}!`)
+        console.log(`musuh mengeluarkan ${masukanMusuh}!`)
         console.log(`anda menang, ${masukanPlayer} menang lawan ${masukanMusuh}`)
     }else if((masukanPlayer==='gunting'&&masukanMusuh==='batu')||(masukanPlayer==='kertas'&&masukanMusuh==='gunting')||(masukanPlayer==='batu'&&masukanMusuh==='kertas')) {
+        console.log(`anda mengeluarkan ${masukanPlayer}!`)
+        console.log(`musuh mengeluarkan ${masukanMusuh}!`)
         console.log(`anda kalah, ${masukanPlayer} kalah lawan ${masukanMusuh}`)
     }else{
-        console.log('input anda ngaco, harusnya input antara gunting, batu, atau kertas')
+        console.log('input anda ngaco. harusnya input antara gunting, batu, atau kertas')
     }
 }
 
-keluaranMusuh('kertas')
+hompimpah('kertas')
