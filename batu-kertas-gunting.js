fd8696c720cc82c09d042526208406f2d91a467d
mittenpuss <42175328+mittenpuss@users.noreply.github.com>
2020-03-02 02:25:34 +0700

    Add files via upload

---
+++ batu-kertas-gunting.js
@@ -0,0 +1,25 @@
+
+// 1 - 3
+const keluaranMusuh=(masukanPlayer)=>{
+    var musuhRandom = Math.ceil(Math.random()*3);
+
+    if(musuhRandom===1){
+        var masukanMusuh='gunting'
+    }else if(musuhRandom===2){
+        masukanMusuh='kertas'
+    }else{
+        masukanMusuh='batu'
+    }
+
+    if((masukanPlayer==='gunting'&&masukanMusuh==='gunting')||(masukanPlayer==='kertas'&&masukanMusuh==='kertas')||(masukanPlayer==='batu'&&masukanMusuh==='batu')){
+        console.log(`anda seri, keduanya sama sama ${masukanPlayer}`)
+    }else if((masukanPlayer==='gunting'&&masukanMusuh==='kertas')||(masukanPlayer==='kertas'&&masukanMusuh==='batu')||(masukanPlayer==='batu'&&masukanMusuh==='gunting')){
+        console.log(`anda menang, ${masukanPlayer} menang lawan ${masukanMusuh}`)
+    }else if((masukanPlayer==='gunting'&&masukanMusuh==='batu')||(masukanPlayer==='kertas'&&masukanMusuh==='gunting')||(masukanPlayer==='batu'&&masukanMusuh==='kertas')) {
+        console.log(`anda kalah, ${masukanPlayer} kalah lawan ${masukanMusuh}`)
+    }else{
+        console.log('input anda ngaco, harusnya input antara gunting, batu, atau kertas')
+    }
+}
+
+keluaranMusuh('kertas')
